65a3234328f9efc9754bc90330a1d33c075da44f
kobzajj <kobzajj93@gmail.com>
2025-10-15 23:25:34 -0700

    began passing stats update

--- data_transformation/src/cfb_analytics/etl/passing.py
+++ data_transformation/src/cfb_analytics/etl/passing.py
@@ -1,6 +1,11 @@
 import pandas as pd, numpy as np
 from ..ep_model import compute_epa, EPModelStub
 
+# columns in pbp file
+# ['season', 'game_id', 'down', 'distance', 'yardline_100', 'yards_gained', 'passer_player_id', 'passer_player_name',
+# 'rusher_player_id', 'rusher_player_name', 'receiver_player_id', 'receiver_player_name', 'is_pass', 'is_rush',
+# 'complete', 'touchdown', 'points_scored', 'interception', 'sack', 'sack_yards']
+
 def assemble_passing(pbp, rosters, parts, season):
     df = pbp.copy()
     df = df[df.get('is_pass', 0)==1].copy()
@@ -14,10 +19,12 @@ def assemble_passing(pbp, rosters, parts, season):
         'interceptions': g['interception'].sum(),
         'sacks_taken': (df.get('sack',0)==1).groupby(df['passer_player_id']).sum(),
         'sacks_yards_lost': (df.get('sack_yards',0)).groupby(df['passer_player_id']).sum() if 'sack_yards' in df.columns else 0,
-        'air_yards': g['air_yards'].sum(min_count=1) if 'air_yards' in df.columns else 0,
-        'yac': g['yac'].sum(min_count=1) if 'yac' in df.columns else 0,
-        'pressures_faced': g['pressure'].sum(min_count=1) if 'pressure' in df.columns else 0,
+        # 'air_yards': g['air_yards'].sum(min_count=1) if 'air_yards' in df.columns else 0, NEED TO ADD AIR YARDS
+        # 'yac': g['yac'].sum(min_count=1) if 'yac' in df.columns else 0, NEED TO ADD YAC
+        # 'pressures_faced': g['pressure'].sum(min_count=1) if 'pressure' in df.columns else 0, NEED TO ADD PRESSURES
     }).fillna(0)
+
+    # CONTINUE FROM HERE
 
     dropbacks = g['is_pass'].sum()
     if 'sack' in df.columns:
